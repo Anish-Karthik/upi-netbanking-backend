24c2dd57d867e07cea9d2c2cf65b418868edc76a
Anish-Karthik <anishkarthik.54321@gmail.com>
2024-10-14 17:21:10 +0530

    fix: 🐛 fixed bug caused by adding annotation processing

--- src/main/java/site/anish_karthik/upi_net_banking/server/utils/FieldFilterUtil.java
+++ src/main/java/site/anish_karthik/upi_net_banking/server/utils/FieldFilterUtil.java
@@ -1,6 +1,8 @@
 package site.anish_karthik.upi_net_banking.server.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import site.anish_karthik.upi_net_banking.server.annotation.IgnoreField;
 
@@ -14,12 +16,29 @@ public class FieldFilterUtil {
             return null;
         }
 
-        ObjectNode node = mapper.valueToTree(data);
-        for (Field field : data.getClass().getDeclaredFields()) {
-            if (field.isAnnotationPresent(IgnoreField.class)) {
-                node.remove(field.getName());
+        JsonNode node = mapper.valueToTree(data);
+        processJsonNode(node, data.getClass());
+        return node;
+    }
+
+    private static void processJsonNode(JsonNode jsonNode, Class<?> clazz) {
+        if (jsonNode.isArray()) {
+            ArrayNode arrayNode = (ArrayNode) jsonNode;
+            for (int i = 0; i < arrayNode.size(); i++) {
+                processJsonNode(arrayNode.get(i), clazz);
+            }
+        } else if (jsonNode.isObject()) {
+            ObjectNode objectNode = (ObjectNode) jsonNode;
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(IgnoreField.class)) {
+                    objectNode.remove(field.getName());
+                } else {
+                    JsonNode childNode = objectNode.get(field.getName());
+                    if (childNode != null) {
+                        processJsonNode(childNode, field.getType());
+                    }
+                }
             }
         }
-        return node;
     }
 }
